9248adbebd7fdf2488664958bb9ff1d7d9c1afcd
Jeremiah S <owner@arouzing.xyz>
2023-06-11 13:46:03 -0400

    Failed to consider certain aspects, user is needed for cloud init to work

--- modules/services/cloud-init.nix
+++ modules/services/cloud-init.nix
@@ -1,6 +1,44 @@
 { config, pkgs, lib, ... }: {
+    # Needed  so cloud init networks work.
+    systemd.network.enable = true;
+
+    # ops should not need password because cloud init automation.
+    security.sudo.extraRules = [
+        { users = [ "ops" ]; command = "ALL"; options = [ "NOPASSWD" "SETENV" ]; }
+    ];
+
+    # ops user needed for cloud init
+    users.users.ops = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ]; 
+    };
+
     services.cloud-init = {
         enable = true;
         network.enable = true;
-    };
+        config = ''
+          system_info:
+            distro: nixos
+            network:
+              renderers: [ 'networkd' ]
+            default_user:
+              name: ops
+          users:
+              - default
+          ssh_pwauth: false
+          chpasswd:
+            expire: false
+          cloud_init_modules:
+            - migrator
+            - seed_random
+            - growpart
+            - resizefs
+          cloud_config_modules:
+            - disk_setup
+            - mounts
+            - set-passwords
+            - ssh
+          cloud_final_modules: []
+          '';
+      };
 }
