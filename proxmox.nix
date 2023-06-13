36942df3ae5249e40c540d8142f1a5311f1daad5
Jermeiah S <owner@arouzing.xyz>
2023-06-13 12:49:13 -0400

    fix typo

--- proxmox.nix
+++ proxmox.nix
@@ -5,7 +5,7 @@ let
 #   rev = "${";
   regex = ".*([0-9]+ [0-9]+\\.[0-9]+).*";
   strippedVersion = builtins.match regex libVersion;
-  proxmoxName = "${releaseVersion}-${if strippedVersion != null then strippedVersion else ""}";
+  proxmoxName = "${releaseVersion}-${if strippedVersion != null then strippedVersion else}";
 in
 {
     proxmox.qemuConf.name = "${proxmoxName}-${self.rev}}";  
