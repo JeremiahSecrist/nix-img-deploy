86e38efdf0b6f875ed07534d2c853ff384b8288f
Jermeiah S <owner@arouzing.xyz>
2023-06-13 13:02:29 -0400

    back to og

--- proxmox.nix
+++ proxmox.nix
@@ -1,13 +1,4 @@
 { config, self, lib, ... }:
-let
-  releaseVersion = builtins.readFile ./release-version;
-  libVersion = lib.version;
-#   rev = "${";
-  regex = ".*([0-9]+ [0-9]+\\.[0-9]+).*";
-  removeSuffix = suffix: str: if lib.strings.hasSuffix suffix str then lib.strings.substring 0 (builtins.length str - builtins.length suffix) str else str;
-  strippedVersion = builtins.match regex libVersion;
-  proxmoxName = "--${removeSuffix "pre-git" libVersion}--";
-in
 {
-    proxmox.qemuConf.name = "${proxmoxName}";  
+    proxmox.qemuConf.name = "${builtins.readFile ./release-version}-${lib.version}-${ self.rev }";  
 }
