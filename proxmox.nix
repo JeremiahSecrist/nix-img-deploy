3f8934d79cf7618cb8beee9ccf22678cafcab390
Jermeiah S <owner@arouzing.xyz>
2023-06-13 12:57:15 -0400

    one more time

--- proxmox.nix
+++ proxmox.nix
@@ -6,7 +6,7 @@ let
   regex = ".*([0-9]+ [0-9]+\\.[0-9]+).*";
   removeSuffix = suffix: str: if lib.strings.hasSuffix suffix str then lib.strings.substring 0 (builtins.length str - builtins.length suffix) str else str;
   strippedVersion = builtins.match regex libVersion;
-  proxmoxName = "--${removeSuffix "pre" libVersion}--";
+  proxmoxName = "--${removeSuffix "pre-git" libVersion}--";
 in
 {
     proxmox.qemuConf.name = "${proxmoxName}";  
